c589339b7e3ee6a56c088383b96f6be9cb42fd01
Vivek Srivastava <viveksrivastava6180@gmail.com>
2021-12-31 04:39:49 +0530

    removed autosave

--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -11,34 +11,43 @@ const Chat = ()=>{
   const [doc,setDoc] = useState(''); 
 
 
-  useEffect(() => {
-    if (socketRef.current == null) return
+  // useEffect(() => {
+  //   if (socketRef.current == null) return
 
-    const interval = setInterval(() => {
-      socketRef.current.emit("saveDoc", doc);
-    }, 200)
+  //   const interval = setInterval(() => {
+  //     socketRef.current.emit("saveDoc", doc);
+  //   }, 200)
 
-    return () => {
-      clearInterval(interval)
-    }
-  }, [doc])
+  //   return () => {
+  //     clearInterval(interval)
+  //   }
+  // }, [doc])
 
   const  onMount = editor => {
     monacoRef.current = editor;
-    socketRef.current = io('http://localhost:5000',{query:search});
-    socketRef.current.on('loadDoc',data=>editor.getModel().setValue(data.data));
+    socketRef.current = io('http://192.168.1.200:5000',{query:search});
+    socketRef.current.on('loadDoc',data=>{
+      console.log(data);
+      //editor.getModel().setValue(data.data);
+    });
     socketRef.current.on('newmsg', data => {
       monacoRef.current.executeEdits(null, [{
         range: data.changes[0].range,
         text: data.changes[0].text,
         forceMoveMarkers: true
       }]);
-   })
+    })
+    socketRef.current.on('giveData',(d)=>{
+      console.log("doc : "+doc)
+      socketRef.current.emit('sendData','vivek');
+    })
   }; 
-  
+  useEffect(()=>{
+    console.log(doc);
+  },[doc]);
+
   const onChange = (v,e) => {
     setDoc(v);
-    console.log(e);
     if(e.changes[0].forceMoveMarkers === true || e.isFlush) return;
     socketRef.current.emit('msg', e);
   }
