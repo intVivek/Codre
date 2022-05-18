29ca38f80a0f37ebe741d6cd82180afecc0655b0
Vivek Srivastava <viveksrivastava6180@gmail.com>
2022-05-19 05:22:44 +0530

    added roombox

--- src/Components/RoomBox/RoomBox.js
+++ src/Components/RoomBox/RoomBox.js
@@ -1,12 +1,19 @@
 import './RoomBox.scss';
+import user from '../../Assets/icons/user.svg';
+import invite from '../../Assets/icons/lock.svg';
+import open from '../../Assets/icons/unlock.svg';
 
 const RoomBox = (props) => {
   
   console.log(props)
   return (
-    <div className='roomBox'>
-      <img className='background' src ={props.data.user.photos[0].value.slice(0, -6)}/>
+    <div className='roomBox' style={{border: `2px solid ${props.data.user.color}`}}>
+      <img className='dp' style={{border: `2px solid ${props.data.user.color}`}} src ={props.data.user.photos[0].value.slice(0,-6)}></img>
+      <div className='top' style={{background: `${props.data.user.color}`}}></div>
       <div className='filter' ></div>
+      <div className='roomName'>{props.data.roomName}</div>
+      <div className='name'><img src={user}/>{props.data.user.name.givenName}</div>
+      <div className='invite'><img src={props.data.invite?invite:open}/>{props.data.invite?'Invite Only':'Open'}</div>
     </div>
   )
 }
